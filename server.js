5ff0249910cdd2b1806204d5ae55c87c815600a1
Fabian <fabianvillagra28@gmail.com>
2024-08-15 14:33:46 -0300

    feat: configuraciones iniciales

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { config } from './src/config/config.js';
+import ConnectDataBase from './src/db/connection.js';
+
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = config.PORT;
+        this.connectDataBase = new ConnectDataBase();
+        this.middlewares();
+    };
+
+    async connectDB() {
+        await this.connectDataBase.connect();
+    };
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+    };
+
+
+
+    async listen() {
+        try {
+            this.app.listen(this.port, () => {
+                console.log(`Server on http://127.0.0.1:${this.port}`);
+            });
+            await this.connectDB();
+        } catch (err) {
+            console.error('Failed to connect to the database', err);
+        }
+    };
+};
+
+
+export default Server;
